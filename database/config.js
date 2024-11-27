16f45216b3d0902778e55c5766b9210a4ee21b7f
Lucia Decima <lu.decima5@gmail.com>
2024-11-27 15:22:17 -0300

    cambio mensaje bd

--- database/config.js
+++ database/config.js
@@ -1,16 +1,33 @@
+// import mongoose from "mongoose";
+
+// const dbConnection = async ()=>{
+
+//     try {
+//         await mongoose.connect(process.env.DATABASE_CNN)
+//         console.log("Base de datos online")
+//     } catch (error) {
+//         throw new Error("Error de conexión a la base de datos");
+        
+        
+//     }
+
+// }
+
+// export {dbConnection};
+
 import mongoose from "mongoose";
 
-const dbConnection = async ()=>{
-
+const dbConnection = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_CNN)
-        console.log("Base de datos online")
+        await mongoose.connect(process.env.DATABASE_CNN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Base de datos conectada correctamente");
     } catch (error) {
+        console.error("Error al conectar la base de datos:", error);
         throw new Error("Error de conexión a la base de datos");
-        
-        
     }
+};
 
-}
-
-export {dbConnection};
+export { dbConnection };
